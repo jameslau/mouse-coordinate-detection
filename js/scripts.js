3e2d0bb69d1b4c977b25d7b87e7e85e599b1c865
jameslau <1402829+jameslau@users.noreply.github.com>
2021-08-05 13:35:56 -0400

    adding mouse detection threshold

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
+
+// set event listener to detect `mousemove`
 document.addEventListener('mousemove', (event) => {
-  // template string for coordinates 
-	log.innerText = (`mouse X: ${event.clientX}, mouse Y: ${event.clientY}`);
+
+  // assign x to the event depending on where clientX is on the browser
+  let x = event.clientX
+	// template string for coordinates
+  log.innerText = (`mouse X: ${event.clientX}, mouse Y: ${event.clientY}`);
+
+  // if mouse is below 500 pixel on the X axis
+  if (x > 500) {
+    // target body and remove class name
+    document.body.className = '';
+    console.log("it's over than 500!");
+    // if mouse is beyond 500 pixel on the X axis
+  } else {
+    // target body and add class name
+    document.body.className = 'xlarge';
+    console.log("it's less than 500!");
+  }
+
 });
 
 // display template string through innerText by aassigning to ID `coords`. 
